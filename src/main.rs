85375b36ffa14d62e14bf9828a28c1d02b74628a
ta.tanaka <takayahilton@gmail.com>
2016-11-06 01:38:43 +0900

    左結合の演算子に対応

--- src/main.rs
+++ src/main.rs
@@ -12,59 +12,189 @@ enum Error {
 }
 
 trait Evaluter: Sized {
-    fn eval(self) -> Result<Self, Error>;
+    fn eval(self) -> Result<Expression, Error>;
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Expression {
-    And(Box<Expression>, Box<Expression>),
-    Bool(bool),
-    Number(u32)
+    Operator(Operator),
+    Value(Value)
 }
 
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Value {
+    Bool(bool),
+    Number(i32)
+}
 
-impl Evaluter for Expression {
+impl Evaluter for Value {
     fn eval(self) -> Result<Expression, Error> {
+        Ok(Expression::Value(self))
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum ContOperator {
+    Add(Expression),
+    Sub(Expression),
+    Mul(Expression)
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Cont {
+    left: Expression,
+    rights: Vec<ContOperator>
+}
+
+impl Cont {
+    fn transform(self) -> Expression {
+        self.rights.iter().fold(self.left, |l, c| match c.clone() {
+            ContOperator::Add(r) =>
+                Expression::Operator(Operator::Add(Box::new(l), Box::new(r))),
+            ContOperator::Sub(r) =>
+                Expression::Operator(Operator::Sub(Box::new(l), Box::new(r))),
+            ContOperator::Mul(r) =>
+                Expression::Operator(Operator::Mul(Box::new(l), Box::new(r)))
+        })
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Operator {
+    And(Box<Expression>, Box<Expression>),
+    Or(Box<Expression>, Box<Expression>),
+    Add(Box<Expression>, Box<Expression>),
+    Sub(Box<Expression>, Box<Expression>),
+    Mul(Box<Expression>, Box<Expression>)
+}
+
+impl Evaluter for Operator {
+    fn eval(self) -> Result<Expression, Error> {
+        use Operator::*;
+        use Expression::Value;
+        use Value::*;
         match self {
-            e@Expression::Bool(_) => Ok(e),
-            e@Expression::Number(_) => Ok(e),
-            Expression::And(l, r) => {
+            And(l, r) => {
                 let left = try!(l.eval());
                 let right = try!(r.eval());
                 match (left, right) {
-                    (Expression::Bool(lb), Expression::Bool(rb)) => Ok(Expression::Bool(lb && rb)),
+                    (Value(Bool(lb)), Value(Bool(rb))) => Bool(lb && rb).eval(),
                     _ => Err(Error::RuntimeError("must bool".to_string()))
+                }
+            },
+            Or(l, r) => {
+                let left = try!(l.eval());
+                let right = try!(r.eval());
+                match (left, right) {
+                    (Value(Bool(lb)), Value(Bool(rb))) => Bool(lb || rb).eval(),
+                    _ => Err(Error::RuntimeError("must bool".to_string()))
+                }
+            },
+            Add(l, r) => {
+                let left = try!(l.eval());
+                let right = try!(r.eval());
+                match (left, right) {
+                    (Value(Number(lb)), Value(Number(rb))) => Number(lb + rb).eval(),
+                    _ => Err(Error::RuntimeError("must number".to_string()))
+                }
+            },
+            Sub(l, r) => {
+                let left = try!(l.eval());
+                let right = try!(r.eval());
+                match (left, right) {
+                    (Value(Number(lb)), Value(Number(rb))) => Number(lb - rb).eval(),
+                    _ => Err(Error::RuntimeError("must number".to_string()))
+                }
+            },
+            Mul(l, r) => {
+                let left = try!(l.eval());
+                let right = try!(r.eval());
+                match (left, right) {
+                    (Value(Number(lb)), Value(Number(rb))) => Number(lb * rb).eval(),
+                    _ => Err(Error::RuntimeError("must number".to_string()))
                 }
             }
         }
     }
 }
 
+
+impl Evaluter for Expression {
+    fn eval(self) -> Result<Expression, Error> {
+        match self {
+            Expression::Value(val) => val.eval(),
+            Expression::Operator(ope) => ope.eval()
+        }
+    }
+}
+
 peg! grammar(r#"
 use super::Expression;
+use super::Operator;
+use super::Value;
+use super::Operator::*;
+use super::Cont;
+use super::ContOperator;
 
 #[pub]
 expression -> Expression
-    = and / bool / number
+    = e10
 
-number -> Expression
-  = [0-9]+ { Expression::Number(match_str.parse().unwrap()) }
+e10 -> Expression
+    = l:e9 rights:e10_cont* { Cont { left: l, rights: rights }.transform() }
+    / e9
 
-white_space
+e10_cont -> ContOperator
+    = space "+" space r:e9 { ContOperator::Add(r) }
+    / space "-" space r:e9 { ContOperator::Sub(r) }
+
+e9 -> Expression
+    = l:e8 rights:e9_cont* { Cont { left: l, rights: rights }.transform() }
+    / e8
+
+e9_cont -> ContOperator
+    = space "*" space r:e8 { ContOperator::Mul(r) }
+
+e8 -> Expression
+    = e7
+
+e7 -> Expression
+    = e6
+
+e6 -> Expression
+    = e5
+
+e5 -> Expression
+    = e4
+
+e4 -> Expression
+    = e3
+
+e3 -> Expression
+    = e2
+
+e2 -> Expression
+    = e1
+
+e1 -> Expression
+    = e0
+
+e0 -> Expression
+    = "(" space e:expression space ")" { e }
+    / v:value { Expression::Value(v) }
+
+value -> Value
+    = number / boolean
+
+number -> Value
+    = [0-9]+ { Value::Number(match_str.parse().unwrap()) }
+
+boolean -> Value
+    = "true" { Value::Bool(true) }
+    / "false" { Value::Bool(false) }
+
+space
     = " "* { () }
-
-and -> Expression
-    = l:bool white_space "&&" white_space r:expression { Expression::And(Box::new(l), Box::new(r)) }
-
-bool -> Expression
-    = literal_true / literal_false
-
-literal_true -> Expression
-  = "true" { Expression::Bool(true) }
-
-literal_false -> Expression
-  = "false" { Expression::Bool(false) }
-
 "#);
 
 fn main() {
